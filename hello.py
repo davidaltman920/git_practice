3bcded9fc8cf75b26e289bbc69d6fa6272485b44
EC2 Default User <ec2-user@ip-172-31-17-228.us-west-1.compute.internal>
2026-05-12 13:36:38 +0000

    local machine commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Hello from both GitHub and local computer!")
 print("This version combines both edits")
-print("Local version: edited locally")
-print("GitHub version: edited on GitHub")
+print("I added this line and removed the two lower ones locally")
+
